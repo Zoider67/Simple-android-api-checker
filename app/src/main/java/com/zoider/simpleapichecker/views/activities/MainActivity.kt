4a246ca34b6d6b5f0981885b7704558fe1e9b7a7
Иван <iw.drozdoff2014@yandex.ru>
2022-02-06 22:37:06 +0300

    initial migrate to jetpack compose

--- app/src/main/java/com/zoider/simpleapichecker/views/activities/MainActivity.kt
+++ app/src/main/java/com/zoider/simpleapichecker/views/activities/MainActivity.kt
@@ -1,60 +1,58 @@
 package com.zoider.simpleapichecker.views.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.NumberPicker
-import com.zoider.simpleapichecker.R
-import com.zoider.simpleapichecker.services.ApiCheckerService
-import java.time.Duration
-import java.time.LocalTime
-import java.util.concurrent.TimeUnit
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     companion object {
         val INTENT_EXTRA_KEY_URL = "url"
         val INTENT_EXTRA_KEY_TIME = "time"
     }
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val startServiceButton = findViewById<Button>(R.id.start_service_button)
+//        val stopServiceButton = findViewById<Button>(R.id.stop_service_button)
+//        val urlEditText = findViewById<EditText>(R.id.url_edit_text)
+//
+//        val hoursPicker: NumberPicker = findViewById(R.id.number_picker_hours)
+//        val minutesPicker: NumberPicker = findViewById(R.id.number_picker_minutes)
+//        val secondsPicker: NumberPicker = findViewById(R.id.number_picker_seconds)
+//
+//        hoursPicker.maxValue = 24
+//        minutesPicker.maxValue = 60
+//        secondsPicker.minValue = 1
+//        secondsPicker.maxValue = 60
+//
+//        minutesPicker.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+//        secondsPicker.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+//
+//        startServiceButton.setOnClickListener {
+//            val intent = Intent(this, ApiCheckerService::class.java)
+//            intent.putExtra(INTENT_EXTRA_KEY_URL, urlEditText.text.toString())
+//            intent.putExtra(
+//                INTENT_EXTRA_KEY_TIME,
+//                Duration.parse("PT${hoursPicker.value}H${minutesPicker.value}M${secondsPicker.value}S")
+//                    .toMillis()
+//            )
+//            val res = applicationContext.startForegroundService(intent)
+//            res.let { Log.d(this.packageName, "starting service: $it") }
+//        }
+//
+//        stopServiceButton.setOnClickListener {
+//            val intent = Intent(this, ApiCheckerService::class.java)
+//            applicationContext.stopService(intent)
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent{
 
-        val startServiceButton = findViewById<Button>(R.id.start_service_button)
-        val stopServiceButton = findViewById<Button>(R.id.stop_service_button)
-        val urlEditText = findViewById<EditText>(R.id.url_edit_text)
-
-        val hoursPicker: NumberPicker = findViewById(R.id.number_picker_hours)
-        val minutesPicker: NumberPicker = findViewById(R.id.number_picker_minutes)
-        val secondsPicker: NumberPicker = findViewById(R.id.number_picker_seconds)
-
-        hoursPicker.maxValue = 24
-        minutesPicker.maxValue = 60
-        secondsPicker.minValue = 1
-        secondsPicker.maxValue = 60
-
-        minutesPicker.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-        secondsPicker.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-
-        startServiceButton.setOnClickListener {
-            val intent = Intent(this, ApiCheckerService::class.java)
-            intent.putExtra(INTENT_EXTRA_KEY_URL, urlEditText.text.toString())
-            intent.putExtra(
-                INTENT_EXTRA_KEY_TIME,
-                Duration.parse("PT${hoursPicker.value}H${minutesPicker.value}M${secondsPicker.value}S")
-                    .toMillis()
-            )
-            val res = applicationContext.startForegroundService(intent)
-            res.let { Log.d(this.packageName, "starting service: $it") }
-        }
-
-        stopServiceButton.setOnClickListener {
-            val intent = Intent(this, ApiCheckerService::class.java)
-            applicationContext.stopService(intent)
         }
     }
 }
