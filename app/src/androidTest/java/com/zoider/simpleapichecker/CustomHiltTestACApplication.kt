e59f6a6a459f1c12b1909ed451e95686ce46dd00
Zoider67 <iw.drozdoff2014@yandex.ru>
2023-01-16 22:22:57 +0300

    #11 Custom hilt test runner

---
+++ app/src/androidTest/java/com/zoider/simpleapichecker/CustomHiltTestACApplication.kt
@@ -0,0 +1,6 @@
+package com.zoider.simpleapichecker
+
+import dagger.hilt.android.testing.CustomTestApplication
+
+@CustomTestApplication(ApiCheckerApplication::class)
+interface CustomHiltTestACApplication
